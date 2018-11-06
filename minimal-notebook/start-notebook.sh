6b877dc4269391363f2ef2370159a3163b292b76
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-11-06 16:40:22 -0500

    Trim down stuff and simplify assembly

--- minimal-notebook/start-notebook.sh
+++ minimal-notebook/start-notebook.sh
@@ -8,29 +8,21 @@ JUPYTER_ENABLE_LAB=`echo "$JUPYTER_ENABLE_LAB" | tr '[A-Z]' '[a-z]'`
 
 if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
     exec /opt/app-root/bin/start-singleuser.sh "$@"
+elif [[ "$JUPYTER_ENABLE_LAB" =~ ^(true|yes|y|1)$ ]]; then
+    exec /opt/app-root/bin/start-lab.sh "$@"
 else
-    if [[ ! -z "${JUPYTER_ENABLE_KERNELGATEWAY}" ]]; then
-        exec /opt/app-root/bin/start-kernelgateway.sh "$@"
-    else
-        if [[ "$JUPYTER_ENABLE_LAB" =~ ^(true|yes|y|1)$ ]]; then
-            exec /opt/app-root/bin/start-lab.sh "$@"
+    if [ x"$JUPYTER_MASTER_FILES" != x"" ]; then
+        if [ x"$JUPYTER_WORKSPACE_NAME" != x"" ]; then
+            JUPYTER_WORKSPACE_PATH=/opt/app-root/src/$JUPYTER_WORKSPACE_NAME
+            setup-volume.sh $JUPYTER_MASTER_FILES $JUPYTER_WORKSPACE_PATH
         fi
     fi
-fi
 
-if [ x"$JUPYTER_MASTER_FILES" != x"" ]; then
-    if [ x"$JUPYTER_WORKSPACE_NAME" != x"" ]; then
-        JUPYTER_WORKSPACE_PATH=/opt/app-root/src/$JUPYTER_WORKSPACE_NAME
-        setup-volume.sh $JUPYTER_MASTER_FILES $JUPYTER_WORKSPACE_PATH
-    fi
-fi
-
-if ! [[ "$JUPYTER_ENABLE_LAB" =~ ^(true|yes|y|1)$ ]]; then
     if [ x"$JUPYTER_WORKSPACE_NAME" != x"" ]; then
         JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS --NotebookApp.default_url=/tree/$JUPYTER_WORKSPACE_NAME"
     fi
+
+    JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS --config=/opt/app-root/etc/jupyter_notebook_config.py"
+
+    exec /opt/app-root/bin/start.sh jupyter notebook $JUPYTER_PROGRAM_ARGS "$@"
 fi
-
-JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS --config=/opt/app-root/etc/jupyter_notebook_config.py"
-
-exec /opt/app-root/bin/start.sh jupyter notebook $JUPYTER_PROGRAM_ARGS "$@"
